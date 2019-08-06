41f5ceba349b40ea853a23e81535fa19e6e5a614
Jonathan Luk <jluk87@gmail.com>
2019-08-06 15:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solutions in this file
-function writeCards (name) {
-
+function writeCards (names, occasion) {
+  for ( let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+  }
 }
 
 function countdown (number) {
